68b41dab8f93a8bc9981c99baa266ea8f0ac4f72
Yang Gao <36057877+ebome@users.noreply.github.com>
2018-10-31 01:01:08 +1100

    Update Replication.py

--- Replication.py
+++ Replication.py
@@ -1,5 +1,6 @@
 import cv2
-
+import math
+from scipy import signal
 from skimage.feature import local_binary_pattern
 import skimage
 import numpy as np
@@ -83,122 +84,117 @@ def Tamura(imag):
     #feature=[0,0,0,0,0]   
     Fcrs = coarseness(imag,kmax=4)
     Fcon = contrast(imag)
-    [Fdir,sita] = directionality(imag)
-    feature = [ Fcrs, Fcon,Fdir ]
+    f = directionality(imag) # return [Fdir,sita]
+    feature = [ Fcrs, Fcon, f[0] ]
     return feature
 
-# 第一个指标 Coarseness，粗糙度
 
-def coarseness(graypic,kmax=4) # graphic为待处理的灰度图像，2^kmax为最大窗口  
+# FIRST: Coarseness
+# returns a float
+def coarseness(graypic,kmax=4): # graphic为待处理的灰度图像，2^kmax为最大窗口  
     height,width = graypic.shape   # 获取图片大小  
-    A=np.zeros(shape=(height,width,2**kmax)); # 平均灰度值矩阵A: 3D matrix 
+    A=np.zeros(shape=(height,width,2**kmax)) # 平均灰度值矩阵A: 3D matrix 
     #计算有效可计算范围内每个点的2^k邻域内的平均灰度值  
     for i in range( (2**(kmax-1)+1), (height-2**(kmax-1)+1) ): # stop is not included in py
         for j in range( (2**(kmax-1)+1), (width-2**(kmax-1)+1) ):
             for k in range(1,kmax):
-                
-                
-                
-                
-                
-                A(i,j,k)=mean2(graypic(i-2**(k-1):i+2**(k-1)-1,j-2**(k-1):j+2**(k-1)-1));  
-     
-        
-        
+                a = anti_norm_image[ (i-2**(k-1)):(i+2**(k-1)) , (j-2**(k-1)):(j+2**(k-1)) ]
+                A[i,j,k] = np.mean(a) # the mean of all the element in matrix
         
     # 对每个像素点计算在水平和垂直方向上不重叠窗口之间的Ak差  
-    for i in range( (2**(kmax-1)+1), (height-2**(kmax-1)+1) ): # stop is not included in py
-        for j in range( (2**(kmax-1)+1), (width-2**(kmax-1)+1) ):
-            for k in range(1,kmax):
-                Eh(i,j,k)=abs(A(i+2**(k-1),j,k)-A(i-2**(k-1),j));  
-                Ev(i,j,k)=abs(A(i,j+2**(k-1),k)-A(i,j-2**(k-1)));  
+    Eh=np.zeros(shape=(height,width,2**kmax)) 
+    Ev=np.zeros(shape=(height,width,2**kmax)) 
+    for i in range( (2**(kmax-1)), (height-2**(kmax-1)) ): # stop is not included in py
+        for j in range( (2**(kmax-1)), (width-2**(kmax-1)) ):
+            for k in range(0,kmax):
+                a = abs(  A[(i+2**(k-1)),j,k] - A[(i-2**(k-1)),j]   ) # a is a vertical list
+                b = abs(  A[i,(j+2**(k-1)),k] - A[i,(j-2**(k-1))]   ) # b is a vertical list
+                Eh[i,j]=a 
+                Ev[i,j]=b  
 
     # 对每个像素点计算使E达到最大值的k  
-    for i in range( (2**(kmax-1)+1), (height-2**(kmax-1)+1) ): # stop is not included in py
-        for j in range( (2**(kmax-1)+1), (width-2**(kmax-1)+1) ):
-            [maxEh,p]=max(Eh(i,j,:));  
-            [maxEv,q]=max(Ev(i,j,:));  
-            if maxEh>maxEv  
-                maxkk=p;  
-            else  
-                maxkk=q;  
-            end  
-        Sbest(i,j)=2**maxkk; # 每个像素点的最优窗口大小为2^maxkk  
-    end  
-end  
+    Sbest=np.zeros(shape=(height,width))
+    maxkk=0
+    for i in range( (2**(kmax-1)), (height-2**(kmax-1)) ): # stop is not included in py
+        for j in range( (2**(kmax-1)), (width-2**(kmax-1)) ):
+            maxEh = max(Eh[i,j,:] )  # Eh[i,j,:] is a vector
+            maxEv = max(Ev[i,j,:] )  
+            if maxEh > maxEv:  
+                maxkk = maxEh  
+            else:  
+                maxkk = maxEv  
+            Sbest[i,j]=2**maxkk # 每个像素点的最优窗口大小为2^maxkk  
+
     # 所有Sbest的均值作为整幅图片的粗糙度  
-    Fcrs=mean2(Sbest);  
-    return Fcrs
+    Fcrs = np.mean(Sbest)  
+    return Fcrs # It is a number
+
+f=coarseness(anti_norm_image)
 
 
-
-
-
-# 第二个指标 Contrast，对比度
+# SECOND: Contrast
 # 注意这个函数因为涉及到方差，要求输入类型为double，因此我这里在源代码上做了适当的修改  
-def contrast(graypic) # graypic为待处理的灰度图片  
-    graypic=double(graypic); # 这一句我自己做了修改，否则原博文中的代码不能直接运行  
-    x=graypic(:); # 二维向量一维化  
-    M4=mean((x-mean(x)).^4); # 四阶矩  
-    delta2=var(x,1); # 方差  
-    alfa4=M4/(delta2**2); # 峰度  
-    delta=std(x,1); # 标准差  
-    Fcon=delta/(alfa4**(1/4)); # 对比度  
-    return Fcon  
+def contrast(graypic): # graypic为待处理的灰度图片  
+    graypic = graypic.astype(float)  
+    x = graypic.reshape(-1)     # Convert 2D arrray in 1D  
+    a=(x-np.mean(x))
+    M4 = np.mean( np.power(a, 4) )  # 四阶矩  
+    delta2 = np.var(x,ddof=1) # 方差  
+    alfa4 = M4/(delta2**2) # 峰度  
+    delta = np.std(x,ddof=1) # 标准差  
+    Fcon = delta/(alfa4**(1/4)) # 对比度  
+    return Fcon   # It is a number
 
 
+# THIRD: Directionality
+# sita为各像素点的角度矩阵，sita will be used in FOURTH:Linelikeness，hence we also return sita  
+def directionality(graypic):  
+    h,w = graypic.shape   # 获取图片大小  
 
-# 第三个指标 Directionality，方向度
-%sita为各像素点的角度矩阵，在线性度中会用到，所以这里作为结果返回  
-function [Fdir,sita]=directionality(graypic)  
-[h w]=size(graypic);  
-%两个方向的卷积矩阵  
-GradientH=[-1 0 1;-1 0 1;-1 0 1];  
-GradientV=[ 1 1 1;0 0 0;-1 -1 -1];  
-%卷积，取有效结果矩阵  
-MHconv=conv2(graypic,GradientH);  
-MH=MHconv(3:h,3:w);  
-MVconv=conv2(graypic,GradientV);  
-MV=MVconv(3:h,3:w);  
-%向量模  
-MG=(abs(MH)+abs(MV))./2;  
-%有效矩阵大小  
-validH=h-2;  
-validW=w-2;  
-%各像素点的方向  
-for i=1:validH  
-    for j=1:validW  
-        sita(i,j)=atan(MV(i,j)/MH(i,j))+(pi/2);  
-    end  
-end  
-n=16;  
-t=12;  
-Nsita=zeros(1,n);  
-%构造方向的统计直方图  
-for i=1:validH  
-    for j=1:validW  
-        for k=1:n  
-            if sita(i,j)>=(2*(k-1)*pi/2/n) && sita(i,j)<((2*(k-1)+1)*pi/2/n) && MG(i,j)>=t  
-                Nsita(k)=Nsita(k)+1;  
-            end  
-        end  
-    end  
-end  
-for k=1:n  
-    HD(k)=Nsita(k)/sum(Nsita(:));  
-end  
-%假设每幅图片只有一个方向峰值，为计算方便简化了原著  
-[maxvalue,FIp]=max(HD);  
-Fdir=0;  
-for k=1:n  
-    Fdir=Fdir+(k-FIp)^2*HD(k);%公式与原著有改动  
-end  
-end  
+    # Sobel Kernels 
+    GradientH = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])  
+    GradientV = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])
+    
+    # Convolution; Get valid Matrix 
+    MHconv = signal.convolve2d(graypic,GradientH)  
+    MH=MHconv[3:(h+1),3:(w+1)]  
+    MVconv=signal.convolve2d(graypic,GradientV) 
+    MV=MVconv[3:(h+1),3:(w+1)] 
+    #向量模  
+    MG=(abs(MH)+abs(MV))/2  
+    
+    # size of valid matrix 
+    validH = h-2  
+    validW = w-2 
+    sita = np.zeros(shape=(validH,validW))
+    MH[MH == 0] = 1 # since we cannot let MV/MH have zero in MH
+    #各像素点的方向  
+    for i in range(0,validH):  
+        for j in range(0,validW):
+            temp=MV[i,j]/MH[i,j]
+            sita[i,j] = np.arctan( temp ) + (np.pi/2)
 
-  
+    n=16  
+    t=12  
+    Nsita = np.zeros(shape=(1,n))  
+    # 构造方向的统计直方图  
+    for i in range(0,validH):  
+        for j in range(0,validW):  
+            for k in range(0,n):  
+                if sita[i,j]>=(2*(k-1)*np.pi/2/n) and sita[i,j]<((2*(k-1)+1)*np.pi/2/n) and MG[i,j]>=t :  
+                    Nsita[:,k]=Nsita[:,k]+1 
+ 
+    HD = np.zeros(shape=(1,n))
+    for k in range(0,n):  
+        HD[:,k]=Nsita[:,k]/(Nsita.sum()) # sum all the elements in Nsita
+ 
+    # HD is histogram 
+    FIp = np.argmax(HD) # Find index of maximum value in HD
+    Fdir=0  
+    for k in range(0,n):  
+        Fdir = Fdir + np.square((k-FIp)) * HD[:,k] # Modification of equations for simpler computation 
 
-
-
+    return [Fdir,sita] 
 
 
 #################################################################################
@@ -220,13 +216,74 @@ plt.show()
 #################################################################################
 # GLCM feature vectors
 
+# Define the maximal grey level
+gray_level = 16
+
+def maxGrayLevel(img_gray):
+    max_gray_level=0
+    (height,width)=img_gray.shape
+    for y in range(height):
+        for x in range(width):
+            if img_gray[y][x] > max_gray_level:
+                max_gray_level = img_gray[y][x]
+    return max_gray_level+1
+
+def getGlcm(img_gray,d_x,d_y):
+    srcdata=img_gray.copy()
+    ret=[[0.0 for i in range(gray_level)] for j in range(gray_level)]
+    (height,width) = img_gray.shape
+
+    max_gray_level=maxGrayLevel(img_gray)
+
+    #若灰度级数大于gray_level，则将图像的灰度级缩小至gray_level，减小灰度共生矩阵的大小
+    if max_gray_level > gray_level:
+        for j in range(height):
+            for i in range(width):
+                srcdata[j][i] = srcdata[j][i]*gray_level / max_gray_level
+
+    for j in range(height-d_y):
+        for i in range(width-d_x):
+             rows = srcdata[j-1][i-1]
+             cols = srcdata[j + d_y-1][i+d_x-1]
+             ret[rows][cols]+=1.0
+
+    for i in range(gray_level):
+        for j in range(gray_level):
+            ret[i][j]/=float(height*width)
+
+    return ret # ret is the 16*16 GLCM 
+
+def feature_computer(p):
+    Con=0.0
+    Eng=0.0
+    Asm=0.0
+    Idm=0.0
+    for i in range(gray_level):
+        for j in range(gray_level):
+            Con+=(i-j)*(i-j)*p[i][j]
+            Asm+=p[i][j]*p[i][j]
+            Idm+=p[i][j]/(1+(i-j)*(i-j))
+            if p[i][j]>0.0:
+                Eng+=p[i][j]*math.log(p[i][j])
+    return Asm,Con,-Eng,Idm
+img_gray= anti_norm_image
+
+glcm_0=getGlcm(img_gray, 1,0)
+glcm_1=getGlcm(img_gray, 0,1)
+glcm_2=getGlcm(img_gray, 1,1)
+glcm_3=getGlcm(img_gray, -1,1)
+
+asm,con,eng,idm=feature_computer(glcm_0)
 
 
+# https://blog.csdn.net/kmsj0x00/article/details/79463376 
 
-
+# Back up  https://blog.csdn.net/qq_23926575/article/details/80599323
 
 #################################################################################
 # Gabor feature vectors
+
+
 
 
 #################################################################################
