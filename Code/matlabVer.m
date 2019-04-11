acfcbac5cfd1beb5c455e0f9b7d78751179f1141
Yang Gao <36057877+ebome@users.noreply.github.com>
2019-04-11 21:50:33 +1000

    Update matlabVer.m

--- Code/matlabVer.m
+++ Code/matlabVer.m
@@ -1,6 +1,5 @@
 %% histogram equalization
-clear
-I=imread('D:\AMME4111\#Code\#Partitioned\#1_clipped.png'); % PNG has 3 channels
+I=imread('D:\AMME4111\#Code\#Partitioned\#121_clipped.png'); % PNG has 3 channels
 K = rgb2gray(I); % Converts RGB channels into greysclae
 
 I_new = mat2gray(K,[0 255]); % Normalize image into [0,1]
@@ -22,22 +21,23 @@ subplot(1,2,2),imshow(DownSampled);title('downsampled image');
 
 % sigma is the radius r, the larger the radius the larger the more unclear the image
 % Size of the Gaussian filter must be odd number, we set (11,11)
-% high sigma = 11; 
-blur_HIGH = imgaussfilt(DownSampled,11,'FilterSize',3); %  Gaussian blur:img_undist=G_h(L)
+% B = imgaussfilt(A,sigma)
+% Size of the Gaussian filter, specified as a scalar or 2-element vector of positive, odd integers. If you specify a scalar, then imgaussfilt uses a square filter.
+blur_HIGH = imgaussfilt(DownSampled,10,'FilterSize',11); %  Gaussian blur:img_undist=G_h(L)
 
-var_0 = DownSampled - blur_HIGH; %  L-G_h(L)
+nominator = DownSampled - blur_HIGH; %  L-G_h(L)
 var_1 = abs(DownSampled - blur_HIGH) ; %  |L-G_h(L)|
-blur_HIGH_denominater = imgaussfilt(DownSampled,11,'FilterSize',3);
+blur_HIGH_denominater = imgaussfilt(var_1,10,'FilterSize',11);
 % blur_HIGH,blur_LOW,var_0,var_1, blur_HIGH_denominater are all matrices with float type
 
 % Prevent 0/0 when do bit-wise division, so if there is 0 in var_0,we will
 % keep this value in var_0 and not change it
-var_3 =  blur_HIGH_denominater./var_0;
+var_3 = nominator./blur_HIGH_denominater;
 var_3(isinf(var_3))=0; 
 
 HIGH_PASS_filter = 0.25*var_3+0.5; % element-wise summation and multiplication
-% high sigma = 5
-BAND_PASS_filter = imgaussfilt(DownSampled,5,'FilterSize',3);
+% low sigma = 5
+BAND_PASS_filter = imgaussfilt(HIGH_PASS_filter,5,'FilterSize',3);
 
 % Up to now, BAND_PASS_filter has value range in [0,1], we can change it to
 % [0,255] using im2uint8 or imadjust. If opposite, use mat2gray
@@ -50,73 +50,63 @@ subplot(122);imshow(Filtered_image);title("Band pass component");
 
 %% texture analysis
 %% 1. Directionality: image gradients along x and y directions
-tamura = Tamura(Filtered_image)
+tamura = Tamura(Filtered_image);
 
 %% 2. LBP
 % Rotation invariance is not very relevant, so set no rotation invariance
-LBP_features=extractLBPFeatures(Filtered_image,'Radius',3,'NumNeighbors',24,'Upright',true);
-[m n]=size(Filtered_image);
-LBPimg=zeros(m,n);
+% Uniformity is not very relevant as well, so ignore the uniformity
+% features = extractLBPFeatures(I) returns extracted uniform local 
 
+% Extract unnormalized LBP features so that we can apply a custom normalization.
+% By default, the normalization method will be L2, but here we choose L1,
+% so set 'Normalization' = 'None'
+% the histogram has 58 separate bins for uniform patterns, 1 bin for all
+% other non-uniform patterns. Hence total 59 bins
+LBP_features=extractLBPFeatures(Filtered_image,'Radius',1,'NumNeighbors',8,'Upright',true,'Normalization','None');
 
-for i=1:m
-    for j=1:n        
-        b0=0;  b1=0;  b2=0;  b3=0;  b4=0;  b5=0;  b6=0;  b7=0;
+% Reshape the LBP features into a number of neighbors -by- number of cells array to access histograms for each individual cell.
+numNeighbors = 8;
+numBins = numNeighbors*(numNeighbors-1)+3;
+lbpCellHists = reshape(LBP_features,numBins,[]);
 
-        if(i-1>0 && j-1>0 && i+1<=m && j+1<=n) % This ensure to ignore the pixels on the edge
-            if(Filtered_image(i-1,j-1)>Filtered_image(i,j))
-                b0=1;
-            end
+% Normalize each LBP cell histogram using L1 norm.
+lbpCellHists = bsxfun(@rdivide,lbpCellHists,sum(lbpCellHists));
+% Reshape the LBP features vector back to 1-by- N feature vector.
+LBP_features = reshape(lbpCellHists,1,[]); % now LBP_features are L1 normalized
 
-            if(Filtered_image(i-1,j)>Filtered_image(i,j))
-                b1=1;
-            end            
-
-            if(Filtered_image(i-1,j+1)>Filtered_image(i,j))
-                b2=1;
-            end           
-
-            if(Filtered_image(i,j+1)>Filtered_image(i,j))
-                b3=1;
-            end            
-
-            if(Filtered_image(i+1,j+1)>Filtered_image(i,j))
-                b4=1;
-            end            
-
-            if(Filtered_image(i+1,j)>Filtered_image(i,j))
-                b5=1;
-            end 
-
-            if(Filtered_image(i+1,j-1)>Filtered_image(i,j))
-                b6=1;
-            end      
-
-            if(Filtered_image(i,j-1)>Filtered_image(i,j))
-                b7=1;
-            end        
-
-            if(Filtered_image(i+1,j-1)>Filtered_image(i,j))
-                b5=1;
-            end              
-        b=b0+b1*2^1+b2*2^2+b3*2^3+b4*2^4+b5*2^5+b6*2^6+b7*2^7;
-        LBPimg(i,j)=b;
-        end
-        
-    end
-end
-
-figure
-subplot(1,2,1),imshow(Filtered_image);title('downsampled image');
-subplot(1,2,2),imshow(LBPimg);title('LBP image');
-
+% so we have 59(the bin number of features/uniform patterns) *64(number of bins) = 3776
+% To reduce the vector size, we can use PCA. Reduce it to 1*8 or other
+% vectors
 %% 3. GLCM 
-glcm = graycomatrix(DownSampled,'NumLevels',256);
-imshow(glcm);
+% Grey-level =256 >  16，we could reduce the level to 16 for reducing computation time
+% NumLevels = Number of gray levels, specified as an integer.
+glcm_4direction = graycomatrix(Filtered_image,'NumLevels',16,'Offset',[0 1; -1 1; -1 0; -1 -1],'Symmetric',true);
+% If your glcm is computed with 'Symmetric' flag you can set the flag 'pairs' to 0
+% the function will normailze GLCM
+glcm_features = GLCM_Features(glcm_4direction,0);
 
-%% 4. Gabor filter
-[mag0, phase0] = imgaborfilt(DownSampled,5,0);
-[mag1, phase1] = imgaborfilt(DownSampled,5,45);
-[mag2, phase2] = imgaborfilt(DownSampled,5,90);
-[mag3, phase3] = imgaborfilt(DownSampled,5,135);
-[mag4, phase4] = imgaborfilt(DownSampled,5,180);
+% Four second-order statistical features for 4 directions
+glcm_4dir_energy = glcm_features.energ;
+glcm_4dir_contrast = glcm_features.contr;
+glcm_4dir_homogenity = glcm_features.homom;
+glcm_4dir_entropy = glcm_features.entro;
+
+% Concatenate the vectors
+GLCM_descriptor = cat(2, glcm_4dir_energy, glcm_4dir_contrast,glcm_4dir_homogenity,glcm_4dir_entropy);
+%% 4. Gabor Response
+% [mag, phase] = imgaborfilt(A,wavelength,orientation)
+% Wavelength of the sinusoidal carrier, specified as a numeric scalar in the range [2,Inf)
+[mag0, phase0] = imgaborfilt(Filtered_image,2,0);
+[mag1, phase1] = imgaborfilt(Filtered_image,2,45);
+[mag2, phase2] = imgaborfilt(Filtered_image,2,90);
+[mag3, phase3] = imgaborfilt(Filtered_image,2,135);
+
+% After getting 12 filter image, use equation 20 in paper to get the E
+
+
+%% 局部模糊模式(Local Fuzzy Patterns, LFP)
+good paper
+http://www.photon.ac.cn/article/2013/1004-4213-42-11-1375.html
+
+
+
